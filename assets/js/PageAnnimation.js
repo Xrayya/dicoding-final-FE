4f72e2f0dce8e6c789b7c03f212d6e68ad19579d
Xrayya <nur.alkhiyar@gmail.com>
2023-10-23 16:18:40 +0700

    feat: set up nav links annimation

---
+++ assets/js/PageAnnimation.js
@@ -0,0 +1,16 @@
+class PageAnnimation {
+  setup() {
+    const navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach((navLink) => {
+      navLink.addEventListener("mouseover", () => {
+        navLink.classList.add("active");
+      });
+
+      navLink.addEventListener("mouseout", () => {
+        navLink.classList.remove("active");
+      });
+    });
+  }
+}
+
+export default PageAnnimation;
---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+import PageAnnimation from "./PageAnnimation.js";
+
+window.addEventListener("load", () => {
+  const pageAnimation = new PageAnnimation();
+  pageAnimation.setup();
+});
